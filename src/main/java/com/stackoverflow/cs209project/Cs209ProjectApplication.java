e2d2d075062dfb588287c4380be5fcf7dc7c68e7
shadowfall09 <120352573+shadowfall09@users.noreply.github.com>
2023-12-11 17:15:16 +0800

    data retrival

---
+++ src/main/java/com/stackoverflow/cs209project/Cs209ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.cs209project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs209ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs209ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stackoverflow/cs209project/Cs209ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.cs209project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs209ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
